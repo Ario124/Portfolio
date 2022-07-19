30fc8dbc9a26a87186a5f0ebd428c204c0c021ac
Ario124 <47329632+Ario124@users.noreply.github.com>
2022-07-19 10:40:03 +0200

    Adjusted placement for text-scroll container and removed unused CSS.

--- base/views.py
+++ base/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 # Create your views here.
 
 def home (request):
-    return render(request, 'base/home.html')
+    item_list = ['Python', 'Django', 'JavaScript', 'SQL', 'CSS3', 'Scripts', 'Heroku']
+    rand_item_list = random.sample(item_list, 7)
+
+    context = {'item_list': rand_item_list}
+    return render(request, 'base/home.html', context)
